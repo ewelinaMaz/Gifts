699b8e4a2476814e8faa1196c998587db04f7ef3
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2020-08-09 21:01:57 +0200

    Add header to main layout

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { Link } from 'react-router-dom';
+import styles from './Header.module.scss';
 import clsx from 'clsx';
-
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import FavoriteIcon  from '@material-ui/icons/Favorite';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
-import styles from './Header.module.scss';
 
-const Component = ({className, children}) => (
+
+const Component = ({className}) => (
   <div className={clsx(className, styles.root)}>
-    <h2>Header</h2>
-    {children}
+    <AppBar className={styles.AppBar}>
+      <Toolbar>
+        <Button edge="start" startIcon= {<FavoriteIcon />} className={styles.menuButton} color="inherit" aria-label="menu">
+          <Link to = '/my-posts'
+            className={styles.login}>
+                Best projects
+          </Link>
+        </Button>
+        <Typography align='center' variant='h4' className={styles.brand}>Special Gifts</Typography>
+        <Button edge="end" color="inherit" startIcon= {<AddShoppingCartIcon />}>
+          <Link 
+            to="/client"
+            className={styles.login}
+          >Shop</Link>
+        </Button>
+      </Toolbar>
+    </AppBar>
   </div>
 );
 
